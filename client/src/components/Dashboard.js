b2515fc93ec73312c472c8527eee1860ae149f23
Bernard Muller <b.mullerjnr@gmail.com>
2021-08-29 17:07:17 +0200

    Fetch Request

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,13 +2,18 @@ import React from 'react'
 
 export default function Dashboard() {
 
-    const meal = fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-    console.log()
+    // const meals = [];
+    // fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    //     .then(res => res.json())
+    //     .then(res => {
+    //         meals.push(res.meals[0])
+    //     });
+
+    
 
     return (
         <div>
-            Dashboard
-            {JSON.stringify(meal)}
+            Recipe:            
         </div>
     )
 }
