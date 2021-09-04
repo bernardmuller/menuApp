f90752f6f48ab74e2dabca224623ff53cb4fc92f
Bernard Muller <b.mullerjnr@gmail.com>
2021-09-04 11:24:23 +0200

    dark/light mode switch

--- client/src/components/Navbar/NavOption.jsx
+++ client/src/components/Navbar/NavOption.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styles from './NavOption.module.css'
 
+import { ThemeContext } from "../../contexts/ThemeContext";
 
-export default function NavOption({ title, darkMode }) {
-
+export default function NavOption({ title }) {
+    const theme = useContext(ThemeContext)
+    const darkMode = theme.state.darkMode;
     return (
         <div className={darkMode ? styles.navOption_dark : styles.navOption_light}>            
             <h3 className={styles.navOption_title}>{title}</h3>
