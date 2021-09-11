ef22277d279a84e219d6e974d885260c9bba22b2
Bernard Muller <b.mullerjnr@gmail.com>
2021-09-11 14:14:33 +0200

    Login validation

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,52 +1,100 @@
-import React, { useRef, useState } from "react"
-import { useAuth } from "../contexts/AuthContext"
-import { Link, useHistory } from "react-router-dom"
-import Card from "./UI/card/Card"
-import styles from './Login.module.css'
+import React, { useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { Link, useHistory } from "react-router-dom";
+import Card from "./UI/card/Card";
+import styles from "./Login.module.css";
+import Button from "./UI/button/Button";
 
-export default function Login() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const { login } = useAuth()
-  const [error, setError] = useState("")
-  const [loading, setLoading] = useState(false)
-  const history = useHistory()
+export default function Login() { 
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [emailMessage, setEmailMessage] = useState("")
+  const [passwordMessage, setPasswordMessage] = useState("")
+  const [invalidEmail, setInvalidEmail] = useState()
+  const [invalidPassword, setInvalidPassword] = useState()
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   async function handleSubmit(e) {
-    e.preventDefault()
-
+    e.preventDefault();
+    
     try {
-      setError("")
-      setLoading(true)
-      await login(emailRef.current.value, passwordRef.current.value)
-      history.push("/dashboard")
-    } catch {
-      setError("Failed to log in")
+      if(invalidEmail || invalidPassword) throw "Invalid username or password"
+      setError("");
+      setLoading(true);
+      await login(enteredEmail, enteredPassword);
+      history.push("/dashboard");
+    } catch(err) {
+      setError(`Failed to log in: ${err}`);
     }
 
-    setLoading(false)
+    setLoading(false);
   }
+
+  const emailChangeHandler = (e) => {
+    setEnteredEmail(e.target.value);  
+
+
+    if(enteredEmail.length > 0) {
+      if(!enteredEmail.includes('@')) {
+        setEmailMessage("A valid email contains an '@'. ")
+        setInvalidEmail(true)
+      } else if(/[!#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]+/.test(enteredEmail)) {
+        setEmailMessage("Special characters not allowed.")
+        setInvalidEmail(true)
+      } else {
+        setEmailMessage("Looks good!")
+        setInvalidEmail(false)
+      }
+    } else {
+      setEmailMessage("")
+      setInvalidEmail(null)
+    }
+    
+  };
+
+  const passwordChangeHandler = (e) => {
+    setEnteredPassword(e.target.value);
+
+    if(enteredPassword.length < 6) {  
+      setPasswordMessage("Password must be longer than 6 characters.")
+      setInvalidPassword(true)
+    } else if (!/\d/.test(enteredPassword)) {
+      setPasswordMessage("Password must contain a number.")
+      setInvalidPassword(true)
+    } else if (!/[A-Z]/.test(enteredPassword)) {
+      setPasswordMessage("Password must contain a capital letter.")
+      setInvalidPassword(true)
+    } else {
+      setPasswordMessage("Looks good!")
+      setInvalidPassword(false)
+    }
+    
+  };
 
   return (
     <>
       <Card className={styles.input}>
         <div>
-          <h2 className="text-center mb-4">Log In</h2>
-          {/* {error && <Alert variant="danger">{error}</Alert>} */}
+          <h2>Log In</h2>
           <form onSubmit={handleSubmit}>
             <div id="email">
-              <div>Email</div>
-              <input type="email" ref={emailRef} required />
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" onChange={emailChangeHandler} required />
+              {emailMessage && <p className={invalidEmail? styles.invalid : styles.valid}>{emailMessage}</p>}
             </div>
             <div id="password">
-              <div>Password</div>
-              <input type="password" ref={passwordRef} required />
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" onChange={passwordChangeHandler} required />
+              {passwordMessage && <p className={invalidPassword? styles.invalid : styles.valid}>{passwordMessage}</p>}
             </div>
-            <button disabled={loading} className="w-100" type="submit">
+            <Button disabled={loading} type="submit">
               Log In
-            </button>
+            </Button>
           </form>
-          <div className="w-100 text-center mt-3">
+          <div>
             <Link to="/forgot-password">Forgot Password?</Link>
           </div>
         </div>
@@ -55,5 +103,5 @@ export default function Login() {
         Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </>
-  )
+  );
 }
